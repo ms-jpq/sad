aca6d9b61d5921389e99b7b293d44af8df432485
Fly me to the moon <git@bigly.dog>
2020-05-08 15:36:50 -0700

    use difflib

--- src/udiff.rs
+++ src/udiff.rs
@@ -1,43 +1,14 @@
-use diff;
-use std::collections::VecDeque;
-
-fn parse(lines: &[diff::Result<&str>], unified_lines: isize) -> Vec<String> {
-  let mut diffs = Vec::<String>::new();
-
-  let mut old_idx = 1;
-  let mut new_idx = 1;
-
-  for line in lines {
-    match line {
-      diff::Result::Left(l) => {
-        let line = format!("-{}\n", l);
-        diffs.push(line);
-        old_idx += 1;
-      }
-      diff::Result::Right(r) => {
-        let line = format!("+{}\n", r);
-        diffs.push(line);
-        new_idx += 1;
-      }
-      diff::Result::Both(l, _) => {
-        let line = format!(" {}\n", l);
-        diffs.push(line);
-        old_idx += 1;
-        new_idx += 1;
-      }
-    };
-  }
-  diffs
-}
+use difflib;
 
 pub fn udiff(name: &str, before: &str, after: &str) -> String {
-  let headers = vec![
-    String::from(format!("diff --git {} {}\n", name, name)),
-    String::from(format!("--- {}\n", name)),
-    String::from(format!("+++ {}\n", name)),
+  let before_split = before.split_terminator("\n").collect::<Vec<&str>>();
+  let after_split = after.split_terminator("\n").collect::<Vec<&str>>();
+  let diff = difflib::unified_diff(&before_split, &after_split, name, name, "", "", 3);
+  let prefix = vec![
+    format!("\ndiff --git {}", name),
+    format!("--- {}", name),
+    format!("+++ {}", name),
   ];
-  let changes = diff::lines(before, after);
-  let diffs = parse(&changes, 3);
-  let print = itertools::chain(headers, diffs);
-  itertools::join(print, "")
+  let print = itertools::chain(prefix, diff.into_iter().skip(2));
+  itertools::join(print, "\n")
 }
