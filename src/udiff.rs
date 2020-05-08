f598987559655ebb5a53e5f9ff4c1a3c0e35eaad
Fly me to the moon <git@bigly.dog>
2020-05-08 15:53:27 -0700

    difflib!

--- src/udiff.rs
+++ src/udiff.rs
@@ -5,10 +5,18 @@ pub fn udiff(name: &str, before: &str, after: &str) -> String {
   let after_split = after.split_terminator("\n").collect::<Vec<&str>>();
   let diff = difflib::unified_diff(&before_split, &after_split, name, name, "", "", 3);
   let prefix = vec![
-    format!("\ndiff --git {}", name),
+    format!("\ndiff --git {} {}", name, name),
     format!("--- {}", name),
     format!("+++ {}", name),
   ];
-  let print = itertools::chain(prefix, diff.into_iter().skip(2));
+  let body = diff
+    .into_iter()
+    .map(|line| match line.chars().next() {
+      Some(ch) if ch == '@' => String::from(line.trim()),
+      _ => line,
+    })
+    .skip(2);
+  let end = vec![String::from("\n")];
+  let print = itertools::chain(itertools::chain(prefix, body), end);
   itertools::join(print, "\n")
 }
