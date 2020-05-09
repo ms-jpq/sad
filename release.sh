b2c2da59e813ec7cfed70c5b3c07a6345d412da9
Fly me to the moon <git@bigly.dog>
2020-05-08 19:41:05 -0700

    release sh

--- release.sh
+++ release.sh
@@ -5,8 +5,22 @@ set -o pipefail
 
 cd "$(dirname "$0")"
 
-mkdir -p ./dist
+mkdir -p ./dist ./out
 
-cargo build --release --target=x86_64-unknown-linux-gnu
-cargo build --release --target=x86_64-apple-darwin
+
+if [[ "$(uname)" = 'Linux' ]]
+then
+  cargo build --release --target=x86_64-unknown-linux-gnu --target-dir=./dist/x86_64-unknown-linux-gnu
+fi
+
+if [[ "$(uname)" = 'Darwin' ]]
+then
+  docker run -it --rm -w /workdir -v "$PWD":/workdir rust cargo build --release --target-dir=./dist/x86_64-unknown-linux-gnu
+  cargo build --release --target-dir=./dist/x86_64-apple-darwin
+
+  cp ./dist/x86_64-apple-darwin/release/sad ./out/x86_64-apple-darwin
+fi
+
+cp ./dist/x86_64-unknown-linux-gnu/release/sad ./out/x86_64-unknown-linux-gnu
+
 
