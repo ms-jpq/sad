009e9101a99ce6ffb8659c2638079c40cace90fc
Fly me to the moon <git@bigly.dog>
2020-05-14 03:06:40 -0700

    use from into

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,5 @@
 use std::{io, string};
+use tokio::sync::mpsc::errors::SendError;
 
 /*
  * Consolidate Error Handling
@@ -11,25 +12,19 @@ pub enum Failure {
   IO(io::Error),
   Str(string::FromUtf8Error),
   Regex(regex::Error),
+  SendError(SendError<()>),
 }
 
 pub type SadResult<T> = Result<T, Failure>;
 
-pub trait Sadness {
-  fn cry(self) -> Failure;
-}
-
-pub trait Depression {
-  type Wry;
-  fn halp(self) -> SadResult<Self::Wry>;
-}
-
-impl<T, E: Sadness> Depression for Result<T, E> {
-  type Wry = T;
-  fn halp(self) -> SadResult<Self::Wry> {
-    match self {
+impl<T, E> From<Result<T, E>> for SadResult<T>
+where
+  E: Into<Failure>,
+{
+  fn from(result: Result<T, E>) -> Self {
+    match result {
       Ok(val) => Ok(val),
-      Err(err) => Err(err.cry()),
+      Err(err) => Err(err.into()),
     }
   }
 }
@@ -38,20 +33,27 @@ impl<T, E: Sadness> Depression for Result<T, E> {
  * Consolidate Error Handling
  */
 
-impl Sadness for io::Error {
-  fn cry(self) -> Failure {
-    Failure::IO(self)
+impl From<io::Error> for Failure {
+  fn from(err: io::Error) -> Self {
+    Failure::IO(err)
   }
 }
 
-impl Sadness for string::FromUtf8Error {
-  fn cry(self) -> Failure {
-    Failure::Str(self)
+impl From<string::FromUtf8Error> for Failure {
+  fn from(err: string::FromUtf8Error) -> Self {
+    Failure::Str(err)
   }
 }
 
-impl Sadness for regex::Error {
-  fn cry(self) -> Failure {
-    Failure::Regex(self)
+impl From<regex::Error> for Failure {
+  fn from(err: regex::Error) -> Self {
+    Failure::Regex(err)
   }
 }
+
+impl<T> From<SendError<T>> for Failure {
+  fn from(err: SendError<T>) -> Self {
+    Failure::Simple(String::from("TODO"))
+  }
+}
+
