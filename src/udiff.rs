8e5c00b9e26d9636fa7a0b5e6290940286f79163
Fly me to the moon <git@bigly.dog>
2020-05-21 16:42:55 -0700

    half way

--- src/udiff.rs
+++ src/udiff.rs
@@ -1,35 +1,86 @@
-use difflib::{self, SequenceMatcher};
+use difflib::sequencematcher::{Opcode, Sequence, SequenceMatcher};
+use std::fmt::Display;
+
+// pub struct Hunk {
+//   pub name: String,
+//   pub range: (usize, usize),
+//   pub lines: Vec<String>,
+// }
+
+fn diff_iter<T: Sequence + Display>(
+  before: &[T],
+  after: &[T],
+  n: usize,
+  new_hunk: impl Fn(&Opcode, &Opcode),
+  eq: impl Fn(&T),
+  plus: impl Fn(&T),
+  minus: impl Fn(&T),
+) {
+  let mut matcher = SequenceMatcher::new(before, after);
+  for group in &matcher.get_grouped_opcodes(n) {
+    let (first, last) = (group.first().unwrap(), group.last().unwrap());
+    new_hunk(first, last);
+    for code in group {
+      if code.tag == "equal" {
+        for item in before.iter().take(code.first_end).skip(code.first_start) {
+          eq(item)
+        }
+        continue;
+      }
+      if code.tag == "replace" || code.tag == "delete" {
+        for item in before.iter().take(code.first_end).skip(code.first_start) {
+          minus(item)
+        }
+      }
+      if code.tag == "replace" || code.tag == "insert" {
+        for item in after.iter().take(code.second_end).skip(code.second_start) {
+          plus(item)
+        }
+      }
+    }
+  }
+}
+
+fn format_range_unified(start: usize, end: usize) -> String {
+  let mut beginning = start + 1;
+  let length = end - start;
+  if length == 1 {
+    return beginning.to_string();
+  }
+  if length == 0 {
+    beginning -= 1;
+  }
+  format!("{},{}", beginning, length)
+}
+
+fn format_unified(first: &Opcode, last: &Opcode) -> String {
+  let r1 = format_range_unified(first.first_start, last.first_end);
+  let r2 = format_range_unified(first.second_start, last.second_end);
+  format!("@@ -{} + {} @@", r1, r2)
+}
 
 pub fn udiff(hunk_size: usize, name: &str, before: &str, after: &str) -> String {
-  let delim = '\n';
-  let before_split = before.split_terminator(delim).collect::<Vec<&str>>();
-  let after_split = after.split_terminator(delim).collect::<Vec<&str>>();
-  let diff = difflib::unified_diff(&before_split, &after_split, name, name, "", "", hunk_size);
-  let prefix = vec![
+  let before_split = before.split_terminator('\n').collect::<Vec<&str>>();
+  let after_split = after.split_terminator('\n').collect::<Vec<&str>>();
+  let mut print = vec![
     format!("\ndiff --git {} {}", name, name),
     format!("--- {}", name),
     format!("+++ {}", name),
   ];
-  let body = diff
-    .into_iter()
-    .map(|line| match line.chars().next() {
-      Some(ch) if ch == '@' => line.trim().into(),
-      _ => line,
-    })
-    .skip(2);
-  let end = vec!["\n".into()];
-  let print = itertools::chain(itertools::chain(prefix, body), end);
-  itertools::join(print, "\n")
-}
-
-pub struct Hunk {
-  name: String,
-  lines: (usize, usize),
-  diff: String,
-}
-
-pub fn hdiff(hunk_size: usize, name: &str, before: &str, after: &str) -> String {
-  let seq = SequenceMatcher::new(before, after);
-
-  return "".into();
+  let new_hunk = |fst: &Opcode, last: &Opcode| {
+    print.push(format_unified(fst, last));
+  };
+  let eq = |line: &&str| {};
+  let plus = |line: &&str| {};
+  let minus = |line: &&str| {};
+  diff_iter(
+    &before_split,
+    &after_split,
+    hunk_size,
+    new_hunk,
+    eq,
+    plus,
+    minus,
+  );
+  print.join("\n")
 }
