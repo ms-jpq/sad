685bf81a266757fdd5d531e3e416e2e90734e460
Fly me to the moon <git@bigly.dog>
2020-05-22 02:19:47 -0700

    udiff

--- src/udiff.rs
+++ src/udiff.rs
@@ -6,17 +6,19 @@ use std::{
   fmt::{self, Display, Formatter},
 };
 
+// WARN: Index starts at 1
 pub struct DiffRange {
   before: (usize, usize),
   after: (usize, usize),
 }
 
 impl DiffRange {
+  // WARN: Opcode Index starts at 0
   pub fn new(ops: &[Opcode]) -> Option<DiffRange> {
     match (ops.first(), ops.last()) {
       (Some(first), Some(last)) => Some(DiffRange {
-        before: (first.first_start, last.first_end),
-        after: (first.second_start, last.second_end),
+        before: (first.first_start + 1, last.first_end - first.first_start),
+        after: (first.second_start + 1, last.second_end - first.second_start),
       }),
       _ => None,
     }
@@ -27,17 +29,10 @@ impl Display for DiffRange {
   fn fmt(&self, f: &mut Formatter) -> fmt::Result {
     write!(
       f,
-      "@@ -{} +{} @@",
-      format_range_unified(self.before),
-      format_range_unified(self.after)
+      "@@ -{},{} +{},{} @@",
+      self.before.0, self.before.1, self.after.0, self.after.1,
     )
   }
-}
-
-fn format_range_unified((start, end): (usize, usize)) -> String {
-  let length = end - start;
-  let beginning = if length == 0 { start } else { start + 1 };
-  format!("{},{}", beginning, length)
 }
 
 impl TryFrom<&str> for DiffRange {
@@ -81,6 +76,60 @@ impl TryFrom<&str> for DiffRange {
   }
 }
 
+pub struct Diff {
+  range: DiffRange,
+  after_lines: Vec<String>,
+}
+
+pub type Diffs = Vec<Diff>;
+
+trait Patchable {
+  fn new(unified: usize, before: &str, after: &str) -> Self;
+  fn patch(&self, before: &[&str]) -> String;
+}
+
+impl Patchable for Diffs {
+  fn new(unified: usize, before: &str, after: &str) -> Self {
+    let before = before.split_terminator('\n').collect::<Vec<&str>>();
+    let after = after.split_terminator('\n').collect::<Vec<&str>>();
+    let mut ret = Vec::new();
+    let mut matcher = SequenceMatcher::new(&before, &after);
+    for group in &matcher.get_grouped_opcodes(unified) {
+      let mut diff = Diff {
+        range: DiffRange::new(group).unwrap(),
+        after_lines: Vec::new(),
+      };
+      for code in group {
+        if code.tag == "replace" || code.tag == "insert" {
+          for line in after.iter().take(code.second_end).skip(code.second_start) {
+            diff.after_lines.push(line.to_string());
+          }
+        }
+      }
+      ret.push(diff);
+    }
+    ret
+  }
+
+  fn patch(&self, before: &[&str]) -> String {
+    let mut ret = String::new();
+    let mut prev = 0;
+    for diff in self.iter() {
+      let (before_start, before_inc) = diff.range.before;
+      let before_end = before_start + before_inc - 1;
+      for i in prev..before_start {
+        ret.push_str(before.get(i).unwrap())
+      }
+      for line in diff.after_lines.iter() {
+        ret.push_str(line)
+      }
+
+      prev = before_end - 1;
+    }
+    ret
+  }
+}
+
 pub fn udiff(unified: usize, name: &str, before: &str, after: &str) -> String {
   let before = before.split_terminator('\n').collect::<Vec<&str>>();
   let after = after.split_terminator('\n').collect::<Vec<&str>>();
@@ -113,4 +162,3 @@ pub fn udiff(unified: usize, name: &str, before: &str, after: &str) -> String {
   }
   ret
 }
-
