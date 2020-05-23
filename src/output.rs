5b265f0dd9d87c5b7fbe9aaa4cc985fff75358fb
Fly me to the moon <git@bigly.dog>
2020-05-23 16:16:13 -0700

    =

--- src/output.rs
+++ src/output.rs
@@ -32,14 +32,14 @@ pub fn stream_output(opts: Options, stream: Receiver<SadResult<String>>) -> Task
   match (opts.action, opts.printer) {
     (Action::Fzf, _) => {
       let preview_args = env::args().collect::<Vec<String>>().join(" ");
-      let execute = format!("abort+execute({}, --internal-patch={{+}})", preview_args);
+      let execute = format!("abort+execute({}, --internal-patch {{+}})", preview_args);
       let mut arguments = vec![
         "--read0".to_string(),
         "-m".to_string(),
         "--ansi".to_string(),
         format!("--bind=enter:{}", execute),
         format!("--bind=double-click:{}", execute),
-        format!("--preview={} --internal-preview {{}}", preview_args),
+        format!("--preview={} --internal-preview={{}}", preview_args),
         "--preview-window=70%:wrap".to_string(),
       ];
       arguments.extend(opts.fzf.unwrap_or_default());
