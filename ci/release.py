b20d3d36859dca43a6e14ba6981e12908c9aac91
Fly me to the moon <git@bigly.dog>
2020-05-25 14:20:58 -0700

    j2 power

--- ci/release.py
+++ ci/release.py
@@ -7,13 +7,16 @@ import subprocess
 import sys
 from argparse import Namespace
 from os import path
-from typing import Iterator, List
+from typing import Any, Callable, Dict, List
 
 import jinja2
 import toml
 import yaml
+from jinja2 import Environment
+
 
 artifacts_dir = "artifacts"
+packages_dir = "packages"
 
 
 def cwd() -> None:
@@ -28,6 +31,36 @@ def run(args: List[str], cwd=os.getcwd()) -> None:
     exit(ret.returncode)
 
 
+def load_yaml(src: str) -> Any:
+  with open(src) as fd:
+    return yaml.safe_load(fd)
+
+
+def load_values() -> Dict[str, str]:
+  cargo = toml.load("Cargo.toml")
+  vals = load_yaml(path.join("ci", "vars.yml"))
+  values = {"project_repo": "https://github.com/ms-jpq/sad",
+            "version": cargo["package"]["version"],
+            "desc": vals["desc"], }
+  return values
+
+
+def build_j2(src: str, filters: Dict[str, Callable] = {}) -> Environment:
+  j2 = jinja2.Environment(
+      enable_async=True,
+      trim_blocks=True,
+      lstrip_blocks=True,
+      undefined=jinja2.StrictUndefined,
+      loader=jinja2.FileSystemLoader(src))
+  j2.filters = {**j2.filters, **filters}
+  return j2
+
+
+def write(filename: str, text: str) -> None:
+  with open(filename, "w") as fd:
+    fd.write(text)
+
+
 def sha256(resource: str) -> str:
   with open(resource, "rb") as fd:
     binary = fd.read()
@@ -35,25 +68,16 @@ def sha256(resource: str) -> str:
     return sha
 
 
-def git_repo(name, uri: str) -> None:
-  install_target = path.join(artifacts_dir, name)
-  if path.isdir(install_target):
-    run(["git", "pull"], cwd=install_target)
-  else:
-    run(["git", "clone", "--depth=1", uri,
-         install_target])
-
-
-def homebrew_release(artifact: str) -> None:
-  if sys.platform != "darwin":
-    return
-  git_repo("homebrew", "https://github.com/ms-jpq/homebrew-sad")
+def homebrew_release(j2: Environment, values: Dict[str, str], artifact: str, uri: str) -> None:
   sha = sha256(artifact)
+  vals = {**values, "sha256": sha, "release_uri": uri}
+  render = j2.get_template("homebrew.rb.j2").render(**vals)
 
 
 def parse_args() -> Namespace:
   parser = argparse.ArgumentParser()
-  parser.add_argument("--homebrew", action="store_true")
+  parser.add_argument("--brew-artifact")
+  parser.add_argument("--brew-uri")
   return parser.parse_args()
 
 
@@ -61,6 +85,14 @@ def main() -> None:
   cwd()
   args = parse_args()
   os.makedirs(artifacts_dir, exist_ok=True)
+  j2 = build_j2(path.join("ci", "templates"))
+  values = load_values()
+  if args.brew_artifact and args.brew_uri:
+    homebrew_release(
+        j2=j2,
+        values=values,
+        artifact=path.join(artifacts_dir, args.brew_artifact),
+        uri=args.brew_uri)
 
 
 main()
