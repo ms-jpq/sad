dbc569423fc32254cd05f56c58fb6ff5d49103ce
Fly me to the moon <git@bigly.dog>
2020-05-24 18:10:24 -0700

    adding in snap

---
+++ release.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse
+import jinja2
+import os
+from typing import List
+from argparse import Namespace
+
+
+def cross_build() -> None:
+  pass
+
+
+def parse_args() -> Namespace:
+  parser = argparse.ArgumentParser()
+  return parser.parse_args()
+
+def main() -> None:
+  args = parse_args()
+  pass
+
