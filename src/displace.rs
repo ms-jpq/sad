1119062d2cab75c90bee46f681ecdb9dcc4fac2d
Fly me to the moon <git@bigly.dog>
2021-08-13 08:00:42 -0700

    evyerhting in threads

--- src/displace.rs
+++ src/displace.rs
@@ -33,11 +33,13 @@ pub async fn displace(opts: &Arc<Options>, payload: Payload) -> Result<String, F
     .as_ref()
     .and_then(|cwd| diff_paths(&path, cwd))
     .unwrap_or_else(|| path.clone());
-  let name = rel_path.display();
+  let name = format!("{}", rel_path.display());
 
   let slurped = slurp(&path).await?;
-  let o = opts.clone();
   let before = Arc::new(slurped.content);
+
+  let o = opts.clone();
+  let o2 = opts.clone();
   let b = before.clone();
   let after = spawn_blocking(move || o.engine.replace(&b)).await?;
 
@@ -45,16 +47,17 @@ pub async fn displace(opts: &Arc<Options>, payload: Payload) -> Result<String, F
     Ok(String::new())
   } else {
     let print = match (&opts.action, payload) {
-      (Action::Preview, Payload::Entire(_)) => udiff(None, opts.unified, &name, &before, &after),
+      (Action::Preview, Payload::Entire(_)) => {
+        spawn_blocking(move || udiff(None, opts.unified, &name, &before, &after)).await?
+      }
       (Action::Preview, Payload::Piecewise(_, ranges)) => {
-        udiff(Some(&ranges), opts.unified, &name, &before, &after)
+        spawn_blocking(move || udiff(Some(&ranges), opts.unified, &name, &before, &after)).await?
       }
       (Action::Commit, Payload::Entire(_)) => {
         spit(&path, &slurped.meta, &after).await?;
         format!("{}\n", name)
       }
       (Action::Commit, Payload::Piecewise(_, ranges)) => {
-        let o2 = opts.clone();
         let after = spawn_blocking(move || {
           let patches = patches(o2.unified, &before, &after);
           apply_patches(patches, &ranges, &before)
@@ -65,8 +68,6 @@ pub async fn displace(opts: &Arc<Options>, payload: Payload) -> Result<String, F
         format!("{}\n", name)
       }
       (Action::Fzf(_, _), _) => {
-        let o2 = opts.clone();
-        let name = format!("{}", name);
         spawn_blocking(move || {
           let ranges = pure_diffs(o2.unified, &before, &after);
           let mut fzf_lines = String::new();
