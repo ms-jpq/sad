d55d801cffe65dee935433f48e0f13d5d1701fa1
Fly me to the moon <git@bigly.dog>
2021-10-03 06:50:08 -0700

    try win

--- build.py
+++ build.py
@@ -28,6 +28,7 @@ _TOOL_CHAINS = {
     "aarch64-unknown-linux-musl",
     "x86_64-apple-darwin",
     "x86_64-pc-windows-gnu",
+    "x86_64-pc-windows-msvc",
     "x86_64-unknown-linux-gnu",
     "x86_64-unknown-linux-musl",
 }
