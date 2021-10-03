7c76e405f36bc2e4a5b7d0257a488c3fd66844b7
Fly me to the moon <git@bigly.dog>
2021-10-03 01:17:54 -0700

    aaaaaaaa

--- ci/release.py
+++ ci/release.py
@@ -1,107 +1,109 @@
 #!/usr/bin/env python3
 
-from argparse import ArgumentParser, Namespace
+from contextlib import contextmanager
+from dataclasses import asdict, dataclass
 from datetime import datetime
 from hashlib import sha256
-from os import environ
-from pathlib import Path, PurePath
-from subprocess import check_call
-from sys import exit
-from typing import Callable, Dict
+from itertools import chain, repeat
+from os import environ, linesep
+from pathlib import Path
+from subprocess import check_call, check_output
+from typing import Iterator
+from urllib.request import build_opener
 
 from jinja2 import Environment, FileSystemLoader, StrictUndefined
 from toml import load as load_toml
 from yaml import safe_load
 
+
+@dataclass(frozen=True)
+class _Project:
+    repo: str
+    version: str
+    desc: str
+    long_desc: str
+
+
 _TOP_LEVEL = Path(__file__).resolve().parent.parent
 
-artifacts_dir = _TOP_LEVEL / "artifacts"
-packages_dir = _TOP_LEVEL / "packages"
 
-
-def _load_values() -> Dict[str, str]:
+def _load_values() -> _Project:
     cargo = load_toml(_TOP_LEVEL / "Cargo.toml")
     vals = safe_load((_TOP_LEVEL / "ci" / "vars.yml").read_text())
-    values = {
-        "project_repo": "https://github.com/ms-jpq/sad",
-        "version": cargo["package"]["version"],
-        "desc": vals["desc"],
-        "long_desc": vals["long_desc"],
-    }
-    return values
+    project = _Project(**{**vals, "version": cargo["package"]["version"]})
+    return project
 
 
-def _build_j2(src: PurePath, filters: Dict[str, Callable] = {}) -> Environment:
+def _release(project: _Project) -> str:
+    tag = environ["GITHUB_REF"].removeprefix("refs/tags/")
+
+    time = datetime.now().strftime("%Y-%m-%d_%H-%M")
+    title = f"ci_{project.version}_{time}"
+    body = (_TOP_LEVEL / "RELEASE_NOTES.md").read_text()
+    message = f"{title}{linesep}{body}"
+
+    attachments = chain.from_iterable(
+        zip(repeat("--attach"), (_TOP_LEVEL / "art").iterdir())
+    )
+
+    check_call(
+        ("hub", "release", "create", "--message", message, *attachments, "--", tag)
+    )
+    a = check_output(
+        ("hub", "release", "show", "--format", "%as", "--", tag), text=True
+    )
+    return a
+
+
+def _build_j2() -> Environment:
     j2 = Environment(
         enable_async=True,
         trim_blocks=True,
         lstrip_blocks=True,
         undefined=StrictUndefined,
-        loader=FileSystemLoader(src),
+        loader=FileSystemLoader(_TOP_LEVEL / "ci" / "templates"),
     )
-    j2.filters = {**j2.filters, **filters}
     return j2
 
 
-def _git_clone(name: PurePath) -> None:
+@contextmanager
+def _git_ops() -> Iterator[Path]:
+    pkgs = _TOP_LEVEL / "packages"
     token = environ["CI_TOKEN"]
+    uri = f"https://ms-jpq:{token}@github.com/ms-jpq/homebrew-sad"
     email = "ci@ci.ci"
     username = "ci-bot"
-    uri = f"https://ms-jpq:{token}@github.com/ms-jpq/homebrew-sad.git"
-    check_call(("git", "clone", "--depth=1", uri, name), cwd=_TOP_LEVEL)
-    check_call(("git", "config", "user.email", email), cwd=name)
-    check_call(("git", "config", "user.name", username), cwd=name)
-
-
-def _git_commit(repo: PurePath) -> None:
     time = datetime.now().strftime("%Y-%m-%d %H:%M")
     msg = f"CI - {time}"
-    check_call(("git", "add", "-A"), cwd=repo)
-    check_call(("git", "commit", "-m", msg), cwd=repo)
-    check_call(("git", "push", "--force"), cwd=repo)
+
+    check_call(("git", "clone", "--depth=1", uri, pkgs), cwd=_TOP_LEVEL)
+    check_call(("git", "config", "user.email", email), cwd=pkgs)
+    check_call(("git", "config", "user.name", username), cwd=pkgs)
+    yield pkgs
+    check_call(("git", "add", "-A"), cwd=pkgs)
+    check_call(("git", "commit", "-m", msg), cwd=pkgs)
+    check_call(("git", "push", "--force"), cwd=pkgs)
 
 
-def _homebrew_release(
-    j2: Environment, values: Dict[str, str], artifact: Path, uri: str
-) -> None:
-    sha = sha256(artifact.read_bytes()).hexdigest()
-    vals = {**values, "sha256": sha, "release_uri": uri}
-    render = j2.get_template("homebrew.rb.j2").render(**vals)
-    (packages_dir / "sad.rb").write_text(render)
-    _git_commit(packages_dir)
+def _template(brew_uri: str, project: _Project) -> None:
+    j2 = _build_j2()
+    with build_opener().open(brew_uri) as resp:
+        brew_artifact = resp.read()
 
+    sha = sha256(brew_artifact).hexdigest()
+    vals = {**asdict(project), "sha256": sha, "release_uri": brew_uri}
+    brew_rend = j2.get_template("homebrew.rb.j2").render(**vals)
+    snap_rend = j2.get_template("snapcraft.yml.j2").render(**vals)
 
-def _snap_release(j2: Environment, values: Dict[str, str]) -> None:
-    vals = {**values}
-    render = j2.get_template("snapcraft.yml.j2").render(**vals)
-    (packages_dir / "snapcraft.yaml").write_text(render)
-    _git_commit(packages_dir)
-
-
-def parse_args() -> Namespace:
-    parser = ArgumentParser()
-    parser.add_argument("--brew-artifact")
-    parser.add_argument("--brew-uri")
-    parser.add_argument("--snapcraft", action="store_true")
-    return parser.parse_args()
+    with _git_ops() as cwd:
+        (cwd / "sad.rb").write_text(brew_rend)
+        (cwd / "snapcraft.yaml").write_text(snap_rend)
 
 
 def main() -> None:
-    args = parse_args()
-    _git_clone(packages_dir)
-    j2 = _build_j2(_TOP_LEVEL / "ci" / "templates")
-    values = _load_values()
-    if args.brew_artifact and args.brew_uri:
-        _homebrew_release(
-            j2=j2,
-            values=values,
-            artifact=artifacts_dir / args.brew_artifact,
-            uri=args.brew_uri,
-        )
-    elif args.snapcraft:
-        _snap_release(j2=j2, values=values)
-    else:
-        exit(1)
+    project = _load_values()
+    brew_uri = _release(project)
+    _template(brew_uri, project=project)
 
 
 main()
