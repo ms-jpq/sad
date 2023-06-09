27ba304a0a607ea9a18372139b5639fa7d5aeb2e
dogdogdog <hao@graveflex.com>
2023-06-08 19:32:39 -0700

    use extern uuidgen

--- build.rs
+++ build.rs
@@ -1,21 +1,23 @@
 #![deny(clippy::all, clippy::cargo, clippy::nursery, clippy::pedantic)]
 #![allow(clippy::cargo_common_metadata, clippy::wildcard_dependencies)]
 
-use uuid::Uuid;
+use std::{
+  error::Error,
+  process::{Command, Stdio},
+};
 
-fn main() {
-  println!(
-    "cargo:rustc-env=SAD_ARGV_UUID={uuid}",
-    uuid = Uuid::new_v4()
-  );
+fn uuid() -> Result<String, Box<dyn Error>> {
+  let out = Command::new("uuidgen").stdout(Stdio::piped()).output()?;
+  assert!(out.status.success());
+  let uuid = String::from_utf8(out.stdout)?.trim().into();
+  Ok(uuid)
+}
 
-  println!(
-    "cargo:rustc-env=SAD_PREVIEW_UUID={uuid}",
-    uuid = Uuid::new_v4()
-  );
+fn main() -> Result<(), Box<dyn Error>> {
+  println!("cargo:rustc-env=SAD_ARGV_UUID={uuid}", uuid = uuid()?);
 
-  println!(
-    "cargo:rustc-env=SAD_PATCH_UUID={uuid}",
-    uuid = Uuid::new_v4()
-  );
+  println!("cargo:rustc-env=SAD_PREVIEW_UUID={uuid}", uuid = uuid()?);
+
+  println!("cargo:rustc-env=SAD_PATCH_UUID={uuid}", uuid = uuid()?);
+  Ok(())
 }
