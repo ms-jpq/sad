e9b64daba72f4233d93ef8ca21dcbfae1947161a
root <github@bigly.dog>
2024-03-23 03:09:07 -0700

    rm arc

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,6 @@ use {
     path::PathBuf,
     pin::pin,
     process::{ExitCode, Termination},
-    sync::Arc,
     thread::available_parallelism,
   },
   subprocess::{stream_into, stream_subproc},
@@ -82,16 +81,12 @@ async fn run(threads: usize) -> Result<(), Die> {
   let (mode, args) = parse_args();
   let input_stream = stream_in(&mode, &args).await;
   let opts = parse_opts(mode, args)?;
-  let options = Arc::new(opts);
-  let opts = options.clone();
-  let trans_stream = input_stream
-    .map_ok(move |input| {
-      let opts = options.clone();
-      async move { displace(&opts, input).await }
-    })
-    .try_buffer_unordered(threads);
 
+  let trans_stream = input_stream
+    .map_ok(|input| displace(&opts, input))
+    .try_buffer_unordered(threads);
   let out_stream = stream_sink(&opts, trans_stream.boxed());
+
   consume(out_stream).await
 }
 
